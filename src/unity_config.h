f111645cc7a8d35b6b3dd21f8b90f74df98e4f85
Dylan Arrabito <darrabito@thoro.ai>
2024-04-04 11:34:37 -0400

    actually remove chibi dep

--- src/unity_config.h
+++ src/unity_config.h
@@ -40,9 +40,6 @@
 #include <stdint.h>
 #include <stdarg.h>
 #include "unity.h"
-#include "board.h"
-#include <ch.h>
-#include <chprintf.h>
 
 /* ************************* AUTOMATIC INTEGER TYPES ***************************
  * C's concept of an integer varies from target to target. The C Standard has
@@ -224,8 +221,8 @@
  * `stdout` option. You decide to route your test result output to a custom
  * serial `RS232_putc()` function you wrote like thus:
  */
-#define UNITY_OUTPUT_CHAR_HEADER_DECLARATION    chprintf(BaseSequentialStream *chp, const char *fmt, ...);
-#define UNITY_OUTPUT_CHAR(a)                    chprintf((BaseSequentialStream *)&(DEBUG_CONSOLE_SD), "%c", a);
+// #define UNITY_OUTPUT_CHAR_HEADER_DECLARATION    chprintf(BaseSequentialStream *chp, const char *fmt, ...);
+// #define UNITY_OUTPUT_CHAR(a)                    chprintf((BaseSequentialStream *)&(DEBUG_CONSOLE_SD), "%c", a);
 //#define UNITY_OUTPUT_FLUSH(s)                   chprintf(DEBUG_CONSOLE_STREAM, "%s", s);
 //#define UNITY_OUTPUT_FLUSH_HEADER_DECLARATION   chprintf(BaseSequentialStream *chp, const char *fmt, ...);
 //#define UNITY_OUTPUT_START()
